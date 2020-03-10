393785f441847d3ccbe79aa06280e46f8278db6b
dnsomoano <dnsomoano@gmail.com>
2020-03-09 22:48:36 -0400

    Added comments to video component.

--- src/Components/Watch.js
+++ src/Components/Watch.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import Axios from "axios";
 
 export class Watch extends Component {
   constructor(props) {
     super(props);
     this.state = {
       channel: "",
+      comments: [],
       description: "",
       id: this.props.match.params.videoId,
       publishDate: "",
@@ -17,26 +19,34 @@ export class Watch extends Component {
   componentDidMount() {
     const url = "https://www.googleapis.com/youtube/v3/";
     const vdResrc = "videos?part=snippet%2CcontentDetails%2Cstatistics";
-    const key = "&key=AIzaSyAJz_naVGZdUyHKo66ByxZO4zNtGW0k2Ng";
-    const apiUrl = url + vdResrc + "&id=" + this.state.id + key;
-    fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        let publishDate = new Date(data.items[0].snippet.publishedAt);
+    const key = "key=AIzaSyAJz_naVGZdUyHKo66ByxZO4zNtGW0k2Ng";
+    const apiUrl = url + vdResrc + "&id=" + this.state.id + "&" + key;
+    // Comments Endpoint
+    const commentsRsrc = "commentThreads";
+    const commentsParameters = "&textFormat=plainText&part=snippet&videoId=";
+    let commentUrl =
+      url + commentsRsrc + "?" + key + commentsParameters + this.state.id;
+    Axios.all([Axios.get(apiUrl), Axios.get(commentUrl)]).then(
+      Axios.spread((dataResponse, commentsResponse) => {
+        let publishDate = new Date(
+          dataResponse.data.items[0].snippet.publishedAt
+        );
         const options = { month: "long" };
         this.setState({
-          channel: data.items[0].snippet.channelTitle,
-          description: data.items[0].snippet.description,
+          channel: dataResponse.data.items[0].snippet.channelTitle,
+          comments: commentsResponse.data.items,
+          description: dataResponse.data.items[0].snippet.description,
           publishDate:
             new Intl.DateTimeFormat("en-US", options).format(publishDate) +
             " " +
             `${publishDate.getDate()}, ${publishDate.getFullYear()}`,
-          title: data.items[0].title,
-          views: data.items[0].statistics.viewCount
+          title: dataResponse.data.items[0].title,
+          views: dataResponse.data.items[0].statistics.viewCount
         });
-      });
-    // console.log(this.state.views);
+        console.log(commentsResponse);
+        // console.log(this.state.comments);
+      })
+    );
   }
 
   render() {
@@ -61,6 +71,48 @@ export class Watch extends Component {
           </div>
           <h4 className="title title-spacing">{this.state.channel}</h4>
           <div>{this.state.description}</div>
+        </div>
+        <h2 className="total-comments-header">Comments</h2>
+        <div>
+          {this.state.comments.map(comment => {
+            return (
+              <div className="comment-thread">
+                <a
+                  href={`${comment.snippet.topLevelComment.snippet.authorChannelUrl}`}
+                >
+                  <img
+                    className="profile-img"
+                    src={`${comment.snippet.topLevelComment.snippet.authorProfileImageUrl}`}
+                    alt={
+                      comment.snippet.topLevelComment.snippet.authorChannelId
+                        .value
+                    }
+                  />
+                </a>
+                <div className="comment-renderer">
+                  <div className="header-container">
+                    <a
+                      className="remove-underline"
+                      href={`${comment.snippet.topLevelComment.snippet.authorChannelUrl}`}
+                    >
+                      <span className="author-header">
+                        {
+                          comment.snippet.topLevelComment.snippet
+                            .authorDisplayName
+                        }
+                      </span>
+                    </a>
+                    <span className="endpoint-color">
+                      {comment.snippet.topLevelComment.snippet.publishedAt}
+                    </span>
+                  </div>
+                  <div>
+                    {comment.snippet.topLevelComment.snippet.textDisplay}
+                  </div>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     );
